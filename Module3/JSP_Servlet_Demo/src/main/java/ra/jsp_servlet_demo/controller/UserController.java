1bb55988aa0b48148f0786f6ade75f717be95c58
Casava2127 <rum01285800687@gmail.com>
2024-11-09 14:54:56 +0700

    Update MD3

---
+++ Module3/JSP_Servlet_Demo/src/main/java/ra/jsp_servlet_demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package ra.jsp_servlet_demo.controller;
+
+
+import ra.jsp_servlet_demo.entity.User;
+import ra.jsp_servlet_demo.service.UserService;
+import ra.jsp_servlet_demo.service.ipm.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/users")
+public class UserController extends HttpServlet {
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<User> users = userService.getAllUsers();
+        request.setAttribute("users", users);
+        request.getRequestDispatcher("users.jsp").forward(request, response);
+    }
+
+    // Implement other HTTP methods (POST, PUT, DELETE) if needed
+}
