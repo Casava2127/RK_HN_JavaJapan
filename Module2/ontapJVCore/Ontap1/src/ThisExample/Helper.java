d2420ce0b51624911ab141a007d2d573174635c4
Casava2127 <rum01285800687@gmail.com>
2025-09-07 17:31:13 +0700

    update

--- Module2/ontapJVCore/Ontap1/src/ThisExample/Helper.java
+++
@@ -1,9 +0,0 @@
-package ThisExample;
-
-class Helper {
-    public static void printStudentInfo(Student s) {
-        System.out.println("Student Info:");
-        System.out.println("- Name: " + s.name);
-        System.out.println("- Age: " + s.age);
-    }
-}
