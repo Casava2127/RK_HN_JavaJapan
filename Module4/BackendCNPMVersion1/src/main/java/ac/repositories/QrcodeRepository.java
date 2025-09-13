84ee42bef0929445b0e3f4c122adde78a9f97dfb
Casava2127 <rum01285800687@gmail.com>
2025-09-13 07:10:43 +0700

    updateC

--- Module4/BackendCNPMVersion1/src/main/java/ac/repositories/QrcodeRepository.java
+++
@@ -1,9 +0,0 @@
-package ac.repositories;
-
-import ac.models.Qrcode;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QrcodeRepository extends JpaRepository<Qrcode, Long> {
-}
